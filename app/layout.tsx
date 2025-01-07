493da6cd83253c80d860b7c7f3cf0e38b2bb3d50
TenCzwarty <jedenzczterech@gmail.com>
2025-01-07 16:13:44 +0100

    chore: add vercel speed insights

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Geist_Mono } from "next/font/google";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 import "./globals.css";
 import Link from "next/link";
 import { Logo } from "../components/logo";
@@ -39,6 +40,8 @@ export default function RootLayout({
             <Link href="#my-writing">my writing</Link>
           </div>
         </footer>
+
+        <SpeedInsights />
       </body>
     </html>
   );
