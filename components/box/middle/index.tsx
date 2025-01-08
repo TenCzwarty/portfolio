d1373fd2db9f3f7108eabe08a213a5281d6ac6c5
TenCzwarty <jedenzczterech@gmail.com>
2025-01-08 15:32:23 +0100

    chore: useMemo for BoxMiddle

--- components/box/middle/index.tsx
+++ components/box/middle/index.tsx
@@ -1,13 +1,18 @@
+import React from "react";
+
 const PAIRS = [
   ["no", "bs"],
   ["da", "bi"],
   ["da", "wo"],
   ["do", "it"],
   ["do", "nt"],
-];
+] as const;
 
 export const BoxMiddle = () => {
-  const [first, second] = PAIRS[Math.floor(Math.random() * PAIRS.length)];
+  const [first, second] = React.useMemo(() => {
+    const randomIndex = Math.floor(Math.random() * PAIRS.length);
+    return PAIRS[randomIndex];
+  }, []);
 
   return (
     <div className="pointer-events-none h-full select-none overflow-hidden rounded-[30%]">
