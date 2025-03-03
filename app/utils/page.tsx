28d9030e19b49f27c600ed264fed8b9dbc6965ba
TenCzwarty <jedenzczterech@gmail.com>
2025-03-03 14:00:23 +0100

    feat: update utils page

--- app/utils/page.tsx
+++ app/utils/page.tsx
@@ -8,7 +8,10 @@ export default function Utils() {
       <Section>
         <div className="mb-8 ml-4 mt-16">
           <h1 className="text-xl font-semibold sm:text-3xl">Utils</h1>
-          <p>place for apps / scripts that do not deserve their own website</p>
+          <p>
+            place for apps / scripts I need that do not deserve their own
+            website
+          </p>
         </div>
 
         <div className="space-y-4">
@@ -20,6 +23,20 @@ export default function Utils() {
               <div className="text-sm">quickly create qr codes, no bs</div>
             </div>
           </Link>
+
+          <div className="flex cursor-not-allowed flex-col rounded-xl border border-neutral-200 p-4">
+            <h3 className="text-lg font-semibold sm:text-xl">
+              Month Time Block
+            </h3>
+            <div className="text-sm">in progress...</div>
+          </div>
+
+          <div className="flex cursor-not-allowed flex-col rounded-xl border border-neutral-200 p-4">
+            <h3 className="text-lg font-semibold sm:text-xl">
+              Secret Santa Generator
+            </h3>
+            <div className="text-sm">in progress...</div>
+          </div>
 
           <div className="flex flex-col rounded-xl border border-neutral-200 p-4">
             <h3 className="text-lg font-semibold sm:text-xl">?</h3>
