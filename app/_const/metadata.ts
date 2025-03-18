4444d655d1017e9136b2b14b7e6c43b5a7589b41
TenCzwarty <jedenzczterech@gmail.com>
2025-03-18 17:06:48 +0100

    feat: animated favicon

--- app/_const/metadata.ts
+++ app/_const/metadata.ts
@@ -4,5 +4,5 @@ export const metadata: Metadata = {
   title: "Daniel Wołoszyn Portfolio",
   description:
     "Portfolio and blog showcasing some of my projects & technical writing.",
-  icons: [{ rel: "icon", url: "/favicon.ico" }],
+  icons: [{ rel: "icon", url: "/favicon.svg", type: "image/svg+xml" }],
 } as const;
